3157ae655c225d04d0ebd731b0f3bd35754562fc
Yunuz Yunuz <112627091+yunkaa19@users.noreply.github.com>
2024-05-23 10:30:08 +0200

    Meditation Controller

---
+++ QuietQuillBE/QuietQuillAPI/Endpoints/Meditation.cs
@@ -0,0 +1,45 @@
+using Application.Meditation.Commands.CreateMeditation;
+using Application.Meditation.Commands.DeleteMeditation;
+using Application.Meditation.Commands.UpdateMeditation;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuietQuillBE.Endpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Meditation : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public Meditation(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpPost("Create"), Authorize]
+        public async Task<IActionResult> CreateEntry([FromBody] CreateMeditationCommand command)
+        {
+            var token = await _mediator.Send(command);
+            return Ok(token);
+        }
+        
+        [HttpPost("Delete"), Authorize]
+        public async Task<IActionResult> DeleteEntry([FromBody] DeleteMeditationCommand command)
+        {
+            var token = await _mediator.Send(command);
+            return Ok(token);
+        }
+
+        [HttpPost("Update"), Authorize]
+        public async Task<IActionResult> UpdateEntry([FromBody] UpdateMeditationCommand command)
+        {
+            var token = await _mediator.Send(command);
+            return Ok(token);
+        }
+        
+        
+    }
+}
